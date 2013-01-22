9756b231bdace42bb2977647110d49d430c46dff
nlpxdc <nlpx_dc@hotmail.com>
2013-01-22 23:05:52 +0800

    config

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Itsite::Application.routes.draw do
+  resources :sites
+
+
   authenticated :user do
-    root :to => 'home#index'
+    root :to => 'sites#index'
   end
-  root :to => "home#index"
+  root :to => 'sites#index'
   devise_for :users
   resources :users
 end
